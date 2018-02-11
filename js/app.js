d3b7b1d3f816e92ba4d37205152a871899bacbfe
Nikola <dzzonii@users.noreply.github.com>
2018-02-12 00:10:14 +0100

    Delay for search input
    Probably not the best solution, but there was issue without delay so that empty search box removes search results and ajax event happen after it, because of ajax loading latency.

--- js/app.js
+++ js/app.js
@@ -29,46 +29,51 @@ $(function (){
 	removeLocal();
 
 	$('#search').keyup(function(){
-		var searchField = $('#search').val();
+		setTimeout(function() {
 
-		// Checks if search field is empty, so it renders items from localstorage and stops ajax request.
-		if(!searchField){
-    		location.reload();
-			return;
-		}
+			var searchField = $('#search').val();
 
-		var url = 'http://gateway.marvel.com/v1/public/characters?ts=1&limit=12&apikey=70e1fe2e617e3a6c5d8e4bb25b54a4bd&hash=4514584efd6c08c6f2a7a30fb0767fb5&nameStartsWith=' + searchField;
-
-		$.ajax({
-			type: 'GET',
-			url: url,
-			dataType: "json",
-			beforeSend: function() {
-				$('.content .loading').show();
-			},
-			success: function (data) {
-				$('.content .loading').hide();
-				$('.content .characters-local').empty();
-		  		characters = data.data.results;
-
-		  		$('.content .search-result').empty();
-			    $.each(data.data.results, function(index){
-			    	// Check if item is already bookmarked
-			    	var alreadyMarked = '';
-			    	for (var i = 0; i < localStorage.length; i++) {
-					    var key = localStorage.key(i);
-						var stringID = JSON.stringify(data.data.results[index].id);
-					    if (key.indexOf(stringID) >= 0) { 
-					        alreadyMarked = 'class="marked"';
-					    }
-					}
-
-			    	$('.content .search-result').append('<li data-index="'+ index +'" '+ alreadyMarked +'><div class="img-container"><img src="' + data.data.results[index].thumbnail.path + '/standard_xlarge.' + data.data.results[index].thumbnail.extension + '"></div><h5>' + data.data.results[index].name + '</h5><button class="mark">+</button><span>Added</span></li>');
-		    	});
-
-
+			// Checks if search field is empty, so it renders items from localstorage and stops ajax request.
+			if(!searchField){
+				$('.content .search-result').empty();
+				localChars();
+				return;
 			}
-		});
+
+			var url = 'http://gateway.marvel.com/v1/public/characters?ts=1&limit=12&apikey=70e1fe2e617e3a6c5d8e4bb25b54a4bd&hash=4514584efd6c08c6f2a7a30fb0767fb5&nameStartsWith=' + searchField;
+
+			$.ajax({
+				type: 'GET',
+				url: url,
+				dataType: "json",
+				beforeSend: function() {
+					$('.search-box .loading').show();
+				},
+				success: function (data) {
+					$('.search-box .loading').hide();
+					$('.content .characters-local').empty();
+			  		characters = data.data.results;
+
+			  		$('.content .search-result').empty();
+				    $.each(data.data.results, function(index){
+				    	// Check if item is already bookmarked
+				    	var alreadyMarked = '';
+				    	for (var i = 0; i < localStorage.length; i++) {
+						    var key = localStorage.key(i);
+							var stringID = JSON.stringify(data.data.results[index].id);
+						    if (key.indexOf(stringID) >= 0) { 
+						        alreadyMarked = 'class="marked"';
+						    }
+						}
+
+				    	$('.content .search-result').append('<li data-index="'+ index +'" '+ alreadyMarked +'><div class="img-container"><img src="' + data.data.results[index].thumbnail.path + '/standard_xlarge.' + data.data.results[index].thumbnail.extension + '"></div><h5>' + data.data.results[index].name + '</h5><button class="mark">+</button><span>Added</span></li>');
+			    	});
+
+
+				}
+			});
+
+        }, 500);
 
 	});
 
